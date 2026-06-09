e1a440d18f09c755cd79355b3ca3ec72d4a4173b
Vito Royeca <vito.royeca@gmail.com>
2026-06-08 23:00:29 -0400

    Added FreeBSD start script

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    server: {
+        https: {
+            key: fs.readFileSync(`./ssl/vitoroyeca_me.key`),
+            cert: fs.readFileSync(`./ssl/vitoroyeca_me.crt`),
+        },
+        // Make sure the server is accessible over the local network
+        host: '0.0.0.0',
+        port: 2443,
+    },
+    plugins: [react()],
 })
